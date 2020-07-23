f7f632d51ad232f5c18d0d50ef43fdd60bc59062
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-23 16:02:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  lowest_value = nil
+  lowest_value = Float::INFINITY
   name_hash.each do |k, v|
     if v < lowest_value
       lowest_value = v
@@ -11,4 +11,4 @@ def key_for_min_value(name_hash)
     end
   end
   lowest_key
-endß
+end
